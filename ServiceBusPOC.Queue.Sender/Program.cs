a519dc74d0a5d7bb9570658c65fae11c44a163e6
Fernando Seguim <fernando.seguim@esx.com.br>
2018-09-21 14:38:33 -0300

    Add project files.

---
+++ ServiceBusPOC.Queue.Sender/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceBusPOC.Queue.Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
